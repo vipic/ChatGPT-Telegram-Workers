493e6336a8d2ac43ca44e24e94eb0b6086a4345c
vipic <read_xms@hotmail.com>
2024-01-30 13:36:16 +0800

    feat: 去掉流式回复，去掉第一个假性响应 可以接受对话后等待，有结果能正确触发完成的通知，而"loading"这个回复没有意义

--- src/llm.js
+++ src/llm.js
@@ -189,51 +189,13 @@ async function requestCompletionsFromLLM(text, context, llm, modifier, onStream)
  * @return {Promise<Response>}
  */
 export async function chatWithLLM(text, context, modifier) {
+  setTimeout(() => sendChatActionToTelegramWithContext(context)('typing').catch(console.error), 0);
   try {
-    try {
-      const msg = await sendMessageToTelegramWithContext(context)(ENV.I18N.message.loading).then((r) => r.json());
-      context.CURRENT_CHAT_CONTEXT.message_id = msg.result.message_id;
-      context.CURRENT_CHAT_CONTEXT.reply_markup = null;
-    } catch (e) {
-      console.error(e);
-    }
-    setTimeout(() => sendChatActionToTelegramWithContext(context)('typing').catch(console.error), 0);
-    let onStream = null;
-    const parseMode = context.CURRENT_CHAT_CONTEXT.parse_mode;
-    if (ENV.STREAM_MODE) {
-      context.CURRENT_CHAT_CONTEXT.parse_mode = null;
-      onStream = async (text) => {
-        try {
-          const resp = await sendMessageToTelegramWithContext(context)(text);
-          if (!context.CURRENT_CHAT_CONTEXT.message_id && resp.ok) {
-            context.CURRENT_CHAT_CONTEXT.message_id = (await resp.json()).result.message_id;
-          }
-        } catch (e) {
-          console.error(e);
-        }
-      };
-    }
-
     const llm = loadChatLLM(context);
     if (llm === null) {
       return sendMessageToTelegramWithContext(context)(`LLM is not enable`);
     }
-    const answer = await requestCompletionsFromLLM(text, context, llm, modifier, onStream);
-    context.CURRENT_CHAT_CONTEXT.parse_mode = parseMode;
-    if (ENV.SHOW_REPLY_BUTTON && context.CURRENT_CHAT_CONTEXT.message_id) {
-      try {
-        await deleteMessageFromTelegramWithContext(context)(context.CURRENT_CHAT_CONTEXT.message_id);
-        context.CURRENT_CHAT_CONTEXT.message_id = null;
-        context.CURRENT_CHAT_CONTEXT.reply_markup={
-          keyboard: [[{text: '/new'}, {text: '/redo'}]],
-          selective: true,
-          resize_keyboard: true,
-          one_time_keyboard: true,
-        };
-      } catch (e) {
-        console.error(e);
-      }
-    }
+    const answer = await requestCompletionsFromLLM(text, context, llm, modifier, null);
     return sendMessageToTelegramWithContext(context)(answer);
   } catch (e) {
     return sendMessageToTelegramWithContext(context)(`Error: ${e.message}`);
